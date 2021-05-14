56decb2408aedd5904e04496ac058795651d78b9
Aminigbo <aminigbopaul@gmail.com>
2021-05-15 00:09:18 +0100

    Aminigbo fixed the bug

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+var form  = document.getElementById('theForm');
 var firstName = document.getElementById("firstname");
 var lastName = document.getElementById("lastname");
 var email = document.getElementById("email");
-var password = document.getElementById("password").val();
-var confirmPassword = document.getElementById("comfirmPassword").val();
+var password = document.getElementById("password")
+var confirmPassword = document.getElementById("confirmPassword")
 var female = document.getElementById("female")
 var male = document.getElementById("male")
 
@@ -13,7 +14,10 @@ function changeLPage(){
     window.location="login.html";
 }
 
-function validateTextbox(){
+
+
+form.addEventListener('submit', function (evnt) {
+    evnt.preventDefault();
     if(firstName.value.length < 3){
         alert("Names must contain aleast 3 characters");    
         firstName.focus();
@@ -27,18 +31,45 @@ function validateTextbox(){
         return false;
     }
     
-    if(password != confirmPassword){
+    if(password.value != confirmPassword.value){
         alert("Password does not match");
         return false;
-    }
+    } 
     
     if(female.checked == false && male.checked == false){
         alert("please choose a gender");
         return false;
     }
-   
     
+    window.location='index.html'
+})
 
+
+
+// function validateTextbox(e) { 
+//     if(firstName.value.length < 3){
+//         alert("Names must contain aleast 3 characters");    
+//         firstName.focus();
+//         firstName.style.border = "solid red";
+//         return false;
+//     }
+//     else if(lastName.value.length < 3){
+//         alert("Names must contain aleast 3 characters");
+//         lastName.focus();
+//         lastName.style.border = "solid red";
+//         return false;
+//     }
     
+//     if(password.value != confirmPassword.value){
+//         alert("Password does not match");
+//         return false;
+//     }
+//     console.log(password.value);
+    
+//     if(female.checked == false && male.checked == false){
+//         alert("please choose a gender");
+//         return false;
+//     }
+  
 
-}
+// }
