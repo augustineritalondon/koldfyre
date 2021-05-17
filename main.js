2cd40e6e940702645b5dbb2c544c4f85e65dd973
augustineritalondon <augustineritalondon@gmail.com>
2021-05-17 21:42:04 +0100

    koldfyre fixed login page, updated register and home page

--- main.js
+++ main.js
@@ -16,52 +16,21 @@ function changeLPage(){
     window.location="login.html";
 }
 
-// form.addEventListener('submit', function (evnt) {
-//     evnt.preventDefault();
-//     if(firstName.value.length < 3){
-//         alert("Names must contain aleast 3 characters");    
-//         firstName.focus(); 
-//         firstName.style.border = "solid red";
-//         return false;
-//     }
-//     else if(lastName.value.length < 3){
-//         alert("Names must contain aleast 3 characters");
-//         lastName.focus();
-//         lastName.style.border = "solid red";
-//         return false;
-//     }
-    
-//     if(password.value != confirmPassword.value){
-//         alert("Password does not match");
-//         return false; 
-//     } 
-    
-//     if(female.checked == false && male.checked == false){
-//         alert("please choose a gender");
-//         return false;
-//     }
-    
-    
-//     window.location='home.html'
-// })
-
 function saveToLocalStorage() {
     localStorage.clear();
 
     let details = {
-        firstname: firstName.value,
-        lastName: lastName.value
+        lFirstName: firstName.value,
+        lLastName: lastName.value,
+        lEmail: email.value,
+        lPassword: password.value,
+        lGender: male.checked ? "male" : "female"
     };
-    //localStorage.setItem("name", firstName.value)
-    //console.log(localStorage);
+
+
    localStorage.setItem("details", JSON.stringify(details));
    console.log(localStorage);  
 }
-
-
-button.addEventListener('click', () => saveToLocalStorage())
-
-// var fromStorage = JSON.parse(localStorage.getItem("details"));
 
 
 
